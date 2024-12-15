c08e6ec7a6a883af893ec1353758cc516c8eb9f1
Nathan Cruz <nathan_cruz77@proton.me>
2024-12-14 20:32:06 -0700

    y fourteen part two -- I'm not proud

--- day_14/part_2.py
+++ day_14/part_2.py
@@ -1,11 +1,37 @@
 from dataclasses import dataclass
-from itertools import product
+from itertools import product, chain
 import re
 import os
 import time
+import array
 
 ROOM_SIZE=(101, 103)
 
+
+def frame_writer(room, n):
+    width = ROOM_SIZE[0]
+    height = ROOM_SIZE[1]
+
+    img = []
+
+    for x in range(width):
+        row = ()
+        
+        for y in range(height):
+            if room[(x, y)] == '#':
+                row += (255, 255, 255)
+            else:
+                row += (0, 0, 0)
+
+        img.append(row)
+
+    img = flatten(img)
+
+    with open(f'frames/frame_{n}.ppm', 'wb') as f:
+        header = f'P6\n{width} {height}\n255\n'
+        f.write(bytearray(header, 'ascii'))
+        f.write(array.array('B', img))
+    
 
 @dataclass
 class Robot:
@@ -56,34 +82,33 @@ def print_room(room):
 
         print()
 
+
+def flatten(list_of_lists):
+    "Flatten one level of nesting."
+    return chain.from_iterable(list_of_lists)
+
     
 def clear(room):
     for k in room:
-        room[k] = ' '
+        room[k] = '.'
 
 
 with open('input.txt') as f:
     robots = [Robot.build(line) for line in f.readlines()]
 
-room = {(x, y): ' ' for x, y in product(range(ROOM_SIZE[0]), range(ROOM_SIZE[1]))}
+room = {(x, y): '.' for x, y in product(range(ROOM_SIZE[0]), range(ROOM_SIZE[1]))}
 
 for robot in robots:
-    robot.move(6_731)
     room[robot.coords] = '#'
 
-seconds = 6_731
-
-while True:
-    print(f'Second: {seconds}')
-    
-    print_room(room)
+for second in range(10_000):
+    frame_writer(room, second)
     clear(room)
 
     for robot in robots:
         robot.move(1)
         room[robot.coords] = '#'
 
-    seconds += 1
+    print(f'{(second/10_000)*100:.3f} => {second}/10_000', end='\r')
 
-    time.sleep(2)
-    os.system('clear')
+print()

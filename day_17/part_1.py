703b95b97c47ce9a5ba924d0285fb0bb5b9abfb2
Nathan Cruz <nathan_cruz77@proton.me>
2024-12-25 18:57:11 -0700

    Day seventeen part one

---
+++ day_17/part_1.py
@@ -0,0 +1,100 @@
+import re
+
+
+def combo(memory, operand):
+    if operand in range(4):
+        return operand
+    
+    if operand == 4:
+        return memory['A']
+
+    if operand == 5:
+        return memory['B']
+
+    if operand == 6:
+        return memory['C']
+    
+    raise 'Unexpected combo operand'
+
+
+def adv(memory, operand):
+    memory['A'] >>= combo(memory, operand)
+
+
+def bxl(memory, operand):
+    memory['B'] ^= operand
+
+
+def bst(memory, operand):
+    memory['B'] = combo(memory, operand) % 8
+
+
+def jnz(memory, operand):
+    if memory['A'] == 0:
+        return
+
+    return operand
+   
+
+def bxc(memory, _operand):
+    memory['B'] ^= memory['C']
+
+
+def out(memory, operand):
+    memory['out'].append(combo(memory, operand) % 8)
+
+
+def bdv(memory, operand):
+    memory['B'] = memory['A'] >> combo(memory, operand)
+
+
+def cdv(memory, operand):
+    memory['C'] = memory['A'] >> combo(memory, operand)
+
+
+def run(memory, program):
+    opcode_function = {
+        0: adv,
+        1: bxl,
+        2: bst,
+        3: jnz,
+        4: bxc,
+        5: out,
+        6: bdv,
+        7: cdv,
+    }
+
+    exec_pointer = 0
+
+    while exec_pointer in range(len(program) - 1):
+        opcode, operand = program[exec_pointer], program[exec_pointer + 1]
+        exec_pointer_jump = opcode_function[opcode](memory, operand)
+        
+        if exec_pointer_jump is not None:
+            exec_pointer = exec_pointer_jump
+        else:
+            exec_pointer += 2
+
+
+with open('input.txt') as f:
+    data = f.read()
+
+    memory_data, program_data = data.split('\n\n')
+
+    memory_data = memory_data.splitlines()
+    program_data = program_data.strip('Program: ')
+
+memory = {}
+
+regex = re.compile(r'(\w): (\d+)')
+
+for line in memory_data:
+    [(register, data)] = regex.findall(line)
+    memory[register] = int(data)
+
+memory['out'] = []
+program = [int(c) for c in program_data.split(',')]
+
+run(memory, program)
+
+print(','.join(str(i) for i in memory['out']))

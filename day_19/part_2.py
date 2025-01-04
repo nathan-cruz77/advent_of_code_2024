9e7bd4575dfcdd9b898f1a2803c1bb1a9ef5708d
Nathan Cruz <nathan_cruz77@proton.me>
2025-01-03 19:12:51 -0700

    Day nineteen part two

---
+++ day_19/part_2.py
@@ -0,0 +1,31 @@
+from functools import cache
+
+@cache
+def backtrack(pattern, towels):
+    if not pattern:
+        return 1
+
+    count = 0
+
+    for towel in towels:
+        if pattern.startswith(towel):
+            count += backtrack(pattern[len(towel):], towels)
+
+    return count
+
+
+with open('input.txt') as f:
+    data = f.read()
+
+    towels_data, patterns_data = data.split('\n\n')
+
+    towels = tuple(towels_data.strip().split(', '))
+    patterns = [l for l in patterns_data.splitlines()]
+
+count = 0
+for index, pattern in enumerate(patterns):
+    print(f'{index + 1} / {len(patterns)}', end='\r')
+    count += backtrack(pattern, towels)
+
+print()
+print(count) 
